7bd7416892f43fe74369599b5ad990d1edf965f8
xiukun.zhou <zhouitpro@gmail.com>
2016-10-23 01:18:08 +0800

    Format code

--- src/Form/NiceMenusSettingsForm.php
+++ src/Form/NiceMenusSettingsForm.php
@@ -40,45 +40,45 @@ class NiceMenusSettingsForm extends ConfigFormBase {
     $config = $this->config('nice_menus.settings');
     // Use JavaScript configuration setting.
     $form['nice_menus_js'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Use JavaScript'),
-      '#description' => $this->t('This will add Superfish jQuery to Nice menus. This is required for Nice menus to work properly in Internet Explorer.'),
+      '#type'          => 'checkbox',
+      '#title'         => $this->t('Use JavaScript'),
+      '#description'   => $this->t('This will add Superfish jQuery to Nice menus. This is required for Nice menus to work properly in Internet Explorer.'),
       '#default_value' => $config->get('nice_menus_js'),
     );
 
     $form['nice_menus_default_css'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Use default css'),
-      '#description' => $this->t('This will add default nice menus style.'),
+      '#type'          => 'checkbox',
+      '#title'         => $this->t('Use default css'),
+      '#description'   => $this->t('This will add default nice menus style.'),
       '#default_value' => $config->get('nice_menus_default_css'),
     );
 
     $form['nice_menus_sf_options'] = array(
-      '#type' => 'fieldset',
-      '#title' => $this->t('Advanced: Superfish options'),
+      '#type'        => 'fieldset',
+      '#title'       => $this->t('Advanced: Superfish options'),
       '#description' => $this->t('You can change the default Superfish options by filling out the desired values here. These only take effect if the Use JavaScript box above is checked.'),
       '#collapsible' => TRUE,
-      '#collapsed' => TRUE,
+      '#collapsed'   => TRUE,
     );
 
     // Mouse delay textfield for the time before the menus is closed.
     $form['nice_menus_sf_options']['nice_menus_sf_delay'] = array(
-      '#type' => 'number',
-      '#title' => $this->t('Mouse delay'),
-      '#description' => $this->t('The delay in milliseconds that the mouse can remain outside a submenu without it closing.'),
+      '#type'          => 'number',
+      '#title'         => $this->t('Mouse delay'),
+      '#description'   => $this->t('The delay in milliseconds that the mouse can remain outside a submenu without it closing.'),
       '#default_value' => $config->get('nice_menus_sf_delay'),
-      '#size' => 5,
+      '#size'          => 5,
     );
 
     // Display speed of the animation for the menu to open/close.
     $form['nice_menus_sf_options']['nice_menus_sf_speed'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Animation speed'),
-      '#description' => $this->t('Speed of the menu open/close animation.'),
-      '#options' => array(
-        'slow' => $this->t('slow'),
+      '#type'          => 'select',
+      '#title'         => $this->t('Animation speed'),
+      '#description'   => $this->t('Speed of the menu open/close animation.'),
+      '#options'       => array(
+        'slow'   => $this->t('slow'),
         'normal' => $this->t('normal'),
-        'fast' => $this->t('fast'),
+        'fast'   => $this->t('fast'),
       ),
       '#default_value' => $config->get('nice_menus_sf_speed'),
     );
@@ -99,11 +99,11 @@ class NiceMenusSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('nice_menus.settings')
-        ->set('nice_menus_sf_delay', $form_state->getValue('nice_menus_sf_delay'))
-        ->set('nice_menus_js', $form_state->getValue('nice_menus_js'))
-        ->set('nice_menus_sf_speed', $form_state->getValue('nice_menus_sf_speed'))
-        ->set('nice_menus_default_css', $form_state->getValue('nice_menus_default_css'))
-        ->save();
+      ->set('nice_menus_sf_delay', $form_state->getValue('nice_menus_sf_delay'))
+      ->set('nice_menus_js', $form_state->getValue('nice_menus_js'))
+      ->set('nice_menus_sf_speed', $form_state->getValue('nice_menus_sf_speed'))
+      ->set('nice_menus_default_css', $form_state->getValue('nice_menus_default_css'))
+      ->save();
     parent::submitForm($form, $form_state);
   }
 }
--- src/Plugin/Block/NiceMenusBlock.php
+++ src/Plugin/Block/NiceMenusBlock.php
@@ -26,36 +26,41 @@ class NiceMenusBlock extends BlockBase implements BlockPluginInterface {
     $form = parent::blockForm($form, $form_state);
     $config = $this->getConfiguration();
     $form['nice_menus_name'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Menu name'),
+      '#type'          => 'textfield',
+      '#title'         => $this->t('Menu name'),
       '#default_value' => isset($config['nice_menus_name']) ? $config['nice_menus_name'] : '',
     );
     $form['nice_menus_menu'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Menu parent'),
-      '#description' => $this->t('The menu parent from which to show a Nice menu.'),
+      '#type'          => 'select',
+      '#title'         => $this->t('Menu parent'),
+      '#description'   => $this->t('The menu parent from which to show a Nice menu.'),
       '#default_value' => isset($config['nice_menus_menu']) ? $config['nice_menus_menu'] : 'navigation:0',
-      '#options' => \Drupal::service('menu.parent_form_selector')->getParentSelectOptions(),
+      '#options'       => \Drupal::service('menu.parent_form_selector')
+        ->getParentSelectOptions(),
     );
     $form['nice_menus_depth'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Menu depth'),
-      '#description' => $this->t('The depth of the menu, i.e. the number of child levels starting with the parent selected above. Leave set to -1 to display all children and use 0 to display no children.'),
+      '#type'          => 'select',
+      '#title'         => $this->t('Menu depth'),
+      '#description'   => $this->t('The depth of the menu, i.e. the number of child levels starting with the parent selected above. Leave set to -1 to display all children and use 0 to display no children.'),
       '#default_value' => isset($config['nice_menus_depth']) ? $config['nice_menus_depth'] : -1,
-      '#options' => array_combine(range(-1, 5), range(-1, 5)),
+      '#options'       => array_combine(range(-1, 5), range(-1, 5)),
     );
     $form['nice_menus_type'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Menu style'),
-      '#description' => $this->t('right: menu items are listed on top of each other and expand to the right') . '<br />' . t('left: menu items are listed on top of each other and expand to the left') . '<br />' . t('down: menu items are listed side by side and expand down'),
+      '#type'          => 'select',
+      '#title'         => $this->t('Menu style'),
+      '#description'   => $this->t('right: menu items are listed on top of each other and expand to the right') . '<br />' . t('left: menu items are listed on top of each other and expand to the left') . '<br />' . t('down: menu items are listed side by side and expand down'),
       '#default_value' => isset($config['nice_menus_type']) ? $config['nice_menus_type'] : 'right',
-      '#options' => array_combine(array('right', 'left', 'down'), array('right', 'left', 'down')),
+      '#options'       => array_combine(array(
+        'right',
+        'left',
+        'down',
+      ), array('right', 'left', 'down')),
     );
     $form['nice_menus_respect_expand'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Respect "Show as expanded" option'),
-      '#description' => $this->t('Menu items have a "Show as expanded" option, which is disabled by default. Since menu items do NOT have this option checked by default, you should choose Yes here once you have configured the menu items that you DO want to expand.'),
-      '#options' => array(
+      '#type'          => 'select',
+      '#title'         => $this->t('Respect "Show as expanded" option'),
+      '#description'   => $this->t('Menu items have a "Show as expanded" option, which is disabled by default. Since menu items do NOT have this option checked by default, you should choose Yes here once you have configured the menu items that you DO want to expand.'),
+      '#options'       => array(
         0 => $this->t('No'),
         1 => $this->t('Yes'),
       ),
@@ -70,27 +75,27 @@ class NiceMenusBlock extends BlockBase implements BlockPluginInterface {
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->setConfiguration([
-      'nice_menus_name' => $form_state->getValue('nice_menus_name'),
-      'nice_menus_menu' => $form_state->getValue('nice_menus_menu'),
-      'nice_menus_depth' => $form_state->getValue('nice_menus_depth'),
-      'nice_menus_type' => $form_state->getValue('nice_menus_type'),
+      'nice_menus_name'           => $form_state->getValue('nice_menus_name'),
+      'nice_menus_menu'           => $form_state->getValue('nice_menus_menu'),
+      'nice_menus_depth'          => $form_state->getValue('nice_menus_depth'),
+      'nice_menus_type'           => $form_state->getValue('nice_menus_type'),
       'nice_menus_respect_expand' => $form_state->getValue('nice_menus_respect_expand'),
     ]);
   }
 
   public function _build_sub_tree($items) {
-    foreach($items as $k => &$item) {
+    foreach ($items as $k => &$item) {
       /**
-      attributes methods.
-      [1] => offsetGet
-      [2] => offsetSet
-      [3] => offsetUnset
-      [4] => offsetExists
-      [5] => addClass
-      [6] => setAttribute
-      [7] => removeAttribute
-      [8] => removeClass
-      [9] => hasClass
+       * attributes methods.
+       * [1] => offsetGet
+       * [2] => offsetSet
+       * [3] => offsetUnset
+       * [4] => offsetExists
+       * [5] => addClass
+       * [6] => setAttribute
+       * [7] => removeAttribute
+       * [8] => removeClass
+       * [9] => hasClass
        */
       if ($item['below']) {
         $item['attributes']->addClass('menuparent');
@@ -135,11 +140,11 @@ class NiceMenusBlock extends BlockBase implements BlockPluginInterface {
     $tree['#items'] = $this->_build_sub_tree($tree['#items']);
 
     return array(
-      '#theme' => 'nice_menus',
-      '#attached' => [
-        'library' => $library
+      '#theme'       => 'nice_menus',
+      '#attached'    => [
+        'library' => $library,
       ],
-      '#menu_output' => drupal_render($tree)
+      '#menu_output' => drupal_render($tree),
     );
   }
 }
