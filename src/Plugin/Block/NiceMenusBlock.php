b3327355f9e5cf1bac5d5b69bef76030bf5ba530
xiukun.zhou <zhouitpro@gmail.com>
2016-10-23 00:12:14 +0800

    Update nice menus support Drupal 8.2

--- src/Plugin/Block/NiceMenusBlock.php
+++ src/Plugin/Block/NiceMenusBlock.php
@@ -1,15 +1,10 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\nice_menus\Plugin\Block\NiceMenusBlock.
- */
-
 namespace Drupal\nice_menus\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Component\Annotation\Plugin;
-use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Block\BlockPluginInterface;
 
 /**
  * Provides a Nice menus block.
@@ -19,79 +14,100 @@ use Drupal\Core\Annotation\Translation;
  *   admin_label = @Translation("Nice menus"),
  * )
  */
-class NiceMenusBlock extends BlockBase {
+class NiceMenusBlock extends BlockBase implements BlockPluginInterface {
 
   /**
-   * {@inheritdoc}
+   * @param array                                $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return array
    */
-  public function blockForm($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
-    $configuration = $this->configuration;
+    $config = $this->getConfiguration();
     $form['nice_menus_name'] = array(
       '#type' => 'textfield',
-      '#title' => t('Menu name'),
-      '#default_value' => isset($configuration['nice_menus_name']) ? $configuration['nice_menus_name'] : '',
+      '#title' => $this->t('Menu name'),
+      '#default_value' => isset($config['nice_menus_name']) ? $config['nice_menus_name'] : '',
     );
     $form['nice_menus_menu'] = array(
       '#type' => 'select',
-      '#title' => t('Menu parent'),
-      '#description' => t('The menu parent from which to show a Nice menu.'),
-      '#default_value' => isset($configuration['nice_menus_menu']) ? $configuration['nice_menus_menu'] : 'navigation:0',
-      '#options' => \Drupal::service('menu.parent_form_selector')->getParentSelectOptions(menu_ui_get_menus()),
+      '#title' => $this->t('Menu parent'),
+      '#description' => $this->t('The menu parent from which to show a Nice menu.'),
+      '#default_value' => isset($config['nice_menus_menu']) ? $config['nice_menus_menu'] : 'navigation:0',
+      '#options' => \Drupal::service('menu.parent_form_selector')->getParentSelectOptions(),
     );
     $form['nice_menus_depth'] = array(
       '#type' => 'select',
-      '#title' => t('Menu depth'),
-      '#description' => t('The depth of the menu, i.e. the number of child levels starting with the parent selected above. Leave set to -1 to display all children and use 0 to display no children.'),
-      '#default_value' => isset($configuration['nice_menus_depth']) ? $configuration['nice_menus_depth'] : -1,
+      '#title' => $this->t('Menu depth'),
+      '#description' => $this->t('The depth of the menu, i.e. the number of child levels starting with the parent selected above. Leave set to -1 to display all children and use 0 to display no children.'),
+      '#default_value' => isset($config['nice_menus_depth']) ? $config['nice_menus_depth'] : -1,
       '#options' => array_combine(range(-1, 5), range(-1, 5)),
     );
     $form['nice_menus_type'] = array(
       '#type' => 'select',
-      '#title' => t('Menu style'),
-      '#description' => t('right: menu items are listed on top of each other and expand to the right') . '<br />' . t('left: menu items are listed on top of each other and expand to the left') . '<br />' . t('down: menu items are listed side by side and expand down'),
-      '#default_value' => isset($configuration['nice_menus_type']) ? $configuration['nice_menus_type'] : 'right',
+      '#title' => $this->t('Menu style'),
+      '#description' => $this->t('right: menu items are listed on top of each other and expand to the right') . '<br />' . t('left: menu items are listed on top of each other and expand to the left') . '<br />' . t('down: menu items are listed side by side and expand down'),
+      '#default_value' => isset($config['nice_menus_type']) ? $config['nice_menus_type'] : 'right',
       '#options' => array_combine(array('right', 'left', 'down'), array('right', 'left', 'down')),
     );
     $form['nice_menus_respect_expand'] = array(
       '#type' => 'select',
-      '#title' => t('Respect "Show as expanded" option'),
-      '#description' => t('Menu items have a "Show as expanded" option, which is disabled by default. Since menu items do NOT have this option checked by default, you should choose Yes here once you have configured the menu items that you DO want to expand.'),
+      '#title' => $this->t('Respect "Show as expanded" option'),
+      '#description' => $this->t('Menu items have a "Show as expanded" option, which is disabled by default. Since menu items do NOT have this option checked by default, you should choose Yes here once you have configured the menu items that you DO want to expand.'),
       '#options' => array(
-        0 => t('No'),
-        1 => t('Yes'),
+        0 => $this->t('No'),
+        1 => $this->t('Yes'),
       ),
-      '#default_value' => isset($configuration['nice_menus_respect_expand']) ? $configuration['nice_menus_respect_expand'] : 0,
+      '#default_value' => isset($config['nice_menus_respect_expand']) ? $config['nice_menus_respect_expand'] : 0,
     );
-
     return $form;
   }
 
   /**
-   * Overrides \Drupal\block\BlockBase::blockSubmit().
+   * @param array                                $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
    */
-  public function blockSubmit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    $this->setConfigurationValue('nice_menus_name', $form_state->getValue('nice_menus_name'));
-    $this->setConfigurationValue('nice_menus_menu', $form_state->getValue('nice_menus_menu'));
-    $this->setConfigurationValue('nice_menus_depth', $form_state->getValue('nice_menus_depth'));
-    $this->setConfigurationValue('nice_menus_type', $form_state->getValue('nice_menus_type'));
-    $this->setConfigurationValue('nice_menus_respect_expand', $form_state->getValue('nice_menus_respect_expand'));
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfiguration([
+      'nice_menus_name' => $form_state->getValue('nice_menus_name'),
+      'nice_menus_menu' => $form_state->getValue('nice_menus_menu'),
+      'nice_menus_depth' => $form_state->getValue('nice_menus_depth'),
+      'nice_menus_type' => $form_state->getValue('nice_menus_type'),
+      'nice_menus_respect_expand' => $form_state->getValue('nice_menus_respect_expand'),
+    ]);
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $nice_menus_menu = isset($this->configuration['nice_menus_menu']) ? $this->configuration['nice_menus_menu'] : 'navigation:0';
-    list($menu_name, $mlid) = explode(':', $nice_menus_menu);
-    $build = array(
+    $block_config = $this->getConfiguration();
+    $nice_menus_menu = isset($block_config['nice_menus_menu']) ? $block_config['nice_menus_menu'] : 'admin:';
+    list($block_config['menu_name'], $block_config['menu_mlid']) = explode(':', $nice_menus_menu);
+
+    $config = \Drupal::config('nice_menus.settings');
+
+    // attach library.
+    $library = [];
+    if ($config->get('nice_menus_js')) {
+      $library[] = 'nice_menus/nice_menus';
+      $library[] = 'nice_menus/superfish';
+      $library[] = 'nice_menus/jquery.hoverIntent';
+      $library[] = 'nice_menus/jquery.bgiframe';
+      $library[] = 'nice_menus/nice_menus_default';
+    }
+
+    $tree = nice_menus_build_tree($block_config);
+
+    $tree['#attributes']['class'][] = 'nice-menu';
+
+    return array(
       '#theme' => 'nice_menus',
-      '#menu_name' => $menu_name,
-      '#mlid' => $mlid,
-      '#direction' => $this->configuration['nice_menus_type'],
-      '#depth' => $this->configuration['nice_menus_depth'],
-      '#respect_expanded' => $this->configuration['nice_menus_respect_expand'],
+      '#attached' => [
+        'library' => $library
+      ],
+      '#menu_output' => drupal_render($tree)
     );
-    return $build;
   }
 }
