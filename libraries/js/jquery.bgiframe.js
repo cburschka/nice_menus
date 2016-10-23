bbfa5cd703c96765cb038a7a5e79c50f0c931b43
xiukun.zhou <zhouitpro@gmail.com>
2016-10-23 12:19:12 +0800

    Issue #2712731 update librarie to last version

--- libraries/js/jquery.bgiframe.js
+++ libraries/js/jquery.bgiframe.js
@@ -1,13 +1,18 @@
-/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
+/*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
  * Licensed under the MIT License (LICENSE.txt).
  *
- * Version 3.0.0
+ * Version 3.0.1
+ *
+ * Requires jQuery >= 1.2.6
  */
 
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS style for Browserify
+        module.exports = factory;
     } else {
         // Browser globals
         factory(jQuery);
@@ -21,7 +26,7 @@
             height      : 'auto', // auto == offsetHeight
             opacity     : true,
             src         : 'javascript:false;',
-            conditional : /MSIE 6.0/.test(navigator.userAgent) // expresion or function. return false to prevent iframe insertion
+            conditional : /MSIE 6\.0/.test(navigator.userAgent) // expresion or function. return false to prevent iframe insertion
         }, s);
 
         // wrap conditional in a function if it isn't already
@@ -31,7 +36,7 @@
         }
 
         var $iframe = $('<iframe class="bgiframe"frameborder="0"tabindex="-1"src="'+s.src+'"'+
-                           'style="display:block;position:absolute;z-index:-1;"/>');
+            'style="display:block;position:absolute;z-index:-1;"/>');
 
         return this.each(function() {
             var $this = $(this);
@@ -40,9 +45,9 @@
             var $el = existing.length === 0 ? $iframe.clone() : existing;
             $el.css({
                 'top': s.top == 'auto' ?
-                    ((parseInt($this.css('borderTopWidth'),10)||0)*-1)+'px' : prop(s.top),
+                ((parseInt($this.css('borderTopWidth'),10)||0)*-1)+'px' : prop(s.top),
                 'left': s.left == 'auto' ?
-                    ((parseInt($this.css('borderLeftWidth'),10)||0)*-1)+'px' : prop(s.left),
+                ((parseInt($this.css('borderLeftWidth'),10)||0)*-1)+'px' : prop(s.left),
                 'width': s.width == 'auto' ? (this.offsetWidth + 'px') : prop(s.width),
                 'height': s.height == 'auto' ? (this.offsetHeight + 'px') : prop(s.height),
                 'opacity': s.opacity === true ? 0 : undefined
